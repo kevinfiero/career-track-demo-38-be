684826080c99fc7cdb930850808729c93e9bba5f
kevinfiero <kjf46@cornell.edu>
2021-02-23 23:02:45 -0800

    complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const ShortUrlService = require('../lib/services/ShortUrlService');
 
 describe('CAREER-TRACK-DEMO-38-BE routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
-  afterEach(() => {
+  afterAll(() => {
     return pool.end();
   });
   it('create a short url via POST', async() => {
@@ -19,6 +20,17 @@ describe('CAREER-TRACK-DEMO-38-BE routes', () => {
           id: expect.stringMatching(/^\w{8}$/),
           originalUrl: 'http://test.com/this/is/very/long' 
         });
+      });
+  });
+
+  it('gets a list of links via GET', async() => {
+    const links = await Promise.all(
+      [...Array(3)].map((_, i) => ShortUrlService.create({ url: `http://test.com/long/url/${i}` })));
+    
+    return request(app)
+      .get('/api/v1/shorten')
+      .then(res => {
+        expect(res.body).toEqual(expect.arrayContaining(links));
       });
   });
 });
